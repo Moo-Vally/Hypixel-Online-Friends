a58914486de8df0944519531bccafc661a6ac1ee
BuildTools <ilanluciano1@gmail.com>
2021-10-03 21:28:18 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Hypixel = require('hypixel-api-reborn');
+const Mojang = require('mojang-api-js');
+const mojang = new Mojang();
+const hypixel = new Hypixel.Client('516cf525-6d40-406a-b463-673a1e668c24')
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setTimeout(100000, () => {
+        console.log('Request has timed out.');
+        res.status(503).send('Service unavailable. Please retry.');
+    });
+    next();
+})
+
+app.post('/friends', (req, res) => {
+    var onlineFriends = [];
+
+    if (req.body.player == null) res.send(['must input player name']);
+
+    hypixel.getFriends(req.body.player).then(friends => {
+        for (let x = 0; x < friends.length; x++) {
+            hypixel.getStatus(friends[x].uuid).then(status => {
+                if (status.online) {
+                    getNameFromUUID(friends[x].uuid).then(friendIgn => {
+                        onlineFriends.push(friendIgn.name);
+                    }).catch(() => {
+                        res.send(['error fetching ign from uuid']);
+                    });
+                }
+
+                if (x == friends.length - 1) {
+                    res.send(onlineFriends);
+                }
+            }).catch(() => {
+                res.send(['error retrieving friends']);
+            });
+        }
+    }).catch(error => {
+        res.send(['error, invalid ign: ' + error]);
+    });
+});
+
+async function getNameFromUUID(uuid) {
+    let friendIgn = await mojang.uuidToName(uuid);
+    return friendIgn
+}
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
