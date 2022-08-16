090de2cb17f1154a136971ca9ab575e6c17b6566
Ilan <ilanluciano1@gmail.com>
2022-08-15 20:39:36 -0700

    many improvements
    in the future should add removing users who get offline from list

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
 const bodyParser = require('body-parser');
 const Hypixel = require('hypixel-api-reborn');
-const Mojang = require('mojang-api-js');
-const mojang = new Mojang();
-const hypixel = new Hypixel.Client('HYPIXEL-API-KEY')
+const hypixel = new Hypixel.Client('089d7117-57d7-4307-8f46-335edae18cd2');
+const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 const port = 3000;
 
 app.use(express.static('public'));
@@ -16,37 +18,30 @@ app.use((req, res, next) => {
         res.status(503).send('Service unavailable. Please retry.');
     });
     next();
-})
+});
 
-app.post('/friends', (req, res) => {
-    var onlineFriends = [];
+io.on('connection', (socket) => {
+    socket.on('getPlayer', (ign) => {
+        if (ign == null) socket.emit('setFriend', 'must input player name');
 
-    if (req.body.player == null) res.send(['must input player name']);
+        hypixel.getFriends(ign).then(friends => {
+            for (let x = 0; x < friends.length; x++) {
+                hypixel.getPlayer(friends[x].uuid).then(player => {
+                    if (player.isOnline) {
+                        socket.emit('setFriend', `[${player.rank}] ${player.nickname}`);
+                    }
 
-    hypixel.getFriends(req.body.player).then(friends => {
-        for (let x = 0; x < friends.length; x++) {
-            hypixel.getPlayer(friends[x].uuid).then(player => {
-                if (player.isOnline) {
-                    onlineFriends.push(player.nickname);
-                }
-
-                if (x == friends.length - 1) {
-                    res.send(onlineFriends);
-                }
-            }).catch(() => {
-                res.send(['error retrieving friends']);
-            });
-        }
-    }).catch(error => {
-        res.send(['error, invalid ign: ' + error]);
+                    if (x == friends.length - 1) socket.emit('done');
+                }).catch(() => {
+                    socket.emit('setFriend', 'error retrieving friends');
+                });
+            }
+        }).catch(error => {
+            socket.emit('setFriend', 'error invalid ign');
+        });
     });
 });
 
-async function getNameFromUUID(uuid) {
-    let friendIgn = await mojang.uuidToName(uuid);
-    return friendIgn
-}
-
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
